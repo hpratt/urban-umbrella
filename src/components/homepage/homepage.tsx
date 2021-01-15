42b3cbf09e3ffae2dd17689e595c508767f125f5
hpratt <hpratt111@live.com>
2021-01-15 11:57:26 -0500

    add homepage search and autocomplete

--- src/components/homepage/homepage.tsx
+++ src/components/homepage/homepage.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
 import { Grid, Container, Divider, Header } from 'semantic-ui-react';
 
 import { Routes } from '../../routes';
@@ -6,37 +7,40 @@ import { Banner } from './banner';
 import { QTLCard, TFBSCard, RECard } from './card';
 import { HomepageSearch } from './search';
 
-const Homepage: React.FC = () => (
-    <div style={{ minHeight: "100%" }}>
-        <Banner />
-        <Container style={{ width: "80%", marginTop: "1em" }}>
-            <Grid>
-                <HomepageSearch onSearchAccept={console.log} />
-                <Grid.Row>
-                    <Divider style={{ width: "100%" }} />
-                </Grid.Row>
-                <Grid.Row>
-                    <Grid.Column width={1} />
-                    <Grid.Column width={14}>
-                        <Header as="h2">
-                            Or, upload BED files or SNP lists to intersect with genomic annotations:
-                        </Header>
-                    </Grid.Column>
-                </Grid.Row>
-                <Grid.Row>
-                    <Grid.Column width={1} />
-                    <Grid.Column width={3}>
-                        <QTLCard link={Routes.qtl()} />
-                    </Grid.Column>
-                    <Grid.Column width={3}>
-                        <TFBSCard link={Routes.tfbs()} />
-                    </Grid.Column>
-                    <Grid.Column width={3}>
-                        <RECard link={Routes.re()} />
-                    </Grid.Column>
-                </Grid.Row>
-            </Grid>
-        </Container>
-    </div>
-);
+const Homepage: React.FC = () => {
+    const [ search, setSearch ] = useState<string | undefined>(undefined);
+    return search ? <Redirect to={`/snp/${search}`} /> : (
+        <div style={{ minHeight: "100%" }}>
+            <Banner />
+            <Container style={{ width: "80%", marginTop: "1em" }}>
+                <Grid>
+                    <HomepageSearch onSearchAccept={setSearch} />
+                    <Grid.Row>
+                        <Divider style={{ width: "100%" }} />
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={1} />
+                        <Grid.Column width={14}>
+                            <Header as="h2">
+                                Or, upload BED files or SNP lists to intersect with genomic annotations:
+                            </Header>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column width={1} />
+                        <Grid.Column width={3}>
+                            <QTLCard link={Routes.qtl()} />
+                        </Grid.Column>
+                        <Grid.Column width={3}>
+                            <TFBSCard link={Routes.tfbs()} />
+                        </Grid.Column>
+                        <Grid.Column width={3}>
+                            <RECard link={Routes.re()} />
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </Container>
+        </div>
+    );
+};
 export default Homepage;
--- src/components/homepage/search/search.tsx
+++ src/components/homepage/search/search.tsx
@@ -1,24 +1,63 @@
-import React, { useState } from 'react';
+import React, { useCallback, useContext, useState } from 'react';
 import { SearchProps } from './types';
-import { Header, Input, Button, Grid } from 'semantic-ui-react';
+import { Header, Button, Grid, Search } from 'semantic-ui-react';
+import { SNP_AUTOCOMPLETE_QUERY } from '../../qtl/queries';
+import { ClientContext } from '../../../App';
+import { SNPWithCoordinates } from '../../qtl/types';
+import { matchGenomicRegion, matchIsGenomicCoordinate } from '../../qtl/page';
 
 export const HomepageSearch: React.FC<SearchProps> = props => {
     const [ search, setSearch ] = useState("");
+    const [ suggestions, setSuggestions ] = useState<any[] | Record<string, any> | undefined>(undefined);
+    const client = useContext(ClientContext);
+
+    const getSuggestions = useCallback(async (_: any, d: any): Promise<any[] | Record<string, any> | undefined> => {
+        return fetch(client, {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                query: SNP_AUTOCOMPLETE_QUERY,
+                variables: { snpid: d.value }
+            })
+        }).then(response => response.json()).then(
+            response => {
+                const results: any[] = response.data.snpAutocompleteQuery.map( (x: SNPWithCoordinates) => ({
+                    title: x.rsId,
+                    description: `${x.coordinates.chromosome}:${x.coordinates.start.toLocaleString()}-${x.coordinates.end.toLocaleString()}`
+                })).slice(0, 3);
+                if (matchIsGenomicCoordinate(matchGenomicRegion(d.value)))
+                    results.push({ title: d.value, description: "genomic coordinates (GRCh38 assembly)" });
+                else if (results.length === 0)
+                    results.push({ title: d.value, description: "(no suggestions)" });
+                return results;
+            }
+        )
+    }, [ client ]);
+
     return (
         <>
             <Grid.Row />
             <Grid.Row>
                 <Grid.Column width={1} />
                 <Grid.Column width={14}>
-                    <Header as="h2">Enter a SNP or genomic region to explore and visualize associated annotations:</Header>
+                    <Header as="h2">Enter a SNP to view associated annotations:</Header>
                 </Grid.Column>
             </Grid.Row>
             <Grid.Row>
                 <Grid.Column width={1} />
-                <Grid.Column width={14}>
-                    <Input style={{ width: "90%" }} icon="search" onChange={e => setSearch(e.target.value)} />&nbsp;
+                <Grid.Column width={13}>
+                    <Search
+                        style={{ width: "100%" }}
+                        icon="search"
+                        onSearchChange={(e: any, d: any) => { setSearch(d.value); getSuggestions(e, d).then(setSuggestions); }}
+                        results={suggestions}
+                        input={{ fluid: true }}
+                        onKeyUp={(e: any) => e.keyCode === 13 && props.onSearchAccept(search)}
+                    /><br />
+                    <em>Examples: rs2838663</em>
+                </Grid.Column>
+                <Grid.Column>
                     <Button onClick={ () => props.onSearchAccept(search) }>Go</Button><br />
-                    <em>Examples: rs2838663, chr1:110,000,000-110,100,000</em>
                 </Grid.Column>
             </Grid.Row>
         </>
